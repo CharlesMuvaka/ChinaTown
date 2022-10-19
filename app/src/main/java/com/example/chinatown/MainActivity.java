a3c0bb0baa94bbd3a8d7cf964f23bc84b488be9b
CharlesMuvaka <charles.uvaka@student.moringaschool.com>
2022-10-19 04:08:36 +0300

    create a binding instance to locate views from the main_activity layout.

--- app/src/main/java/com/example/chinatown/MainActivity.java
+++ app/src/main/java/com/example/chinatown/MainActivity.java
@@ -1,14 +1,24 @@
 package com.example.chinatown;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
 
 import android.os.Bundle;
 
+import com.example.chinatown.databinding.ActivityMainBinding;
+
 public class MainActivity extends AppCompatActivity {
+    ActivityMainBinding mainBinding;
+    NavController navController;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        mainBinding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(mainBinding.getRoot());
+
+
+
+
     }
 }
